addeee7fb1b075680c0de73e72473ce542eb90bd
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-04-13 09:21:15 +0900

    feat: get number of today commits from github

--- app/src/main/java/kr/hs/dimigo/everyday_commits/MainActivity.java
+++ app/src/main/java/kr/hs/dimigo/everyday_commits/MainActivity.java
@@ -1,13 +1,43 @@
 package kr.hs.dimigo.everyday_commits;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = (TextView) findViewById(R.id.tvResult);
+
+        try {
+            StrictMode.ThreadPolicy policy = new
+                    StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            Document document = Jsoup.connect("https://www.github.com/JunhoYeo").get();
+            Element rect = document.select("rect").last();
+
+            int commits = Integer.parseInt(rect.attr("data-count"));
+            System.out.println(commits);
+
+            if (commits == 0)
+                result.setText("오늘 커밋 없음");
+            else
+                result.setText("오늘 커밋\t" + commits + "개");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
